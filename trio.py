00a923f95c5e729fa5e5cdcd821a56ce788a4c07
Sigurjón Ísaksson <sigurjonisaksson@Sigurjons-MacBook-Pro.local>
2017-03-23 16:42:17 +0000

    updated

--- trio.py
+++ trio.py
@@ -5,15 +5,11 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("vcf", type = str)
-parser.add_argument("trio", type = str)
-parser.add_argument("twins", type = str)
+parser.add_argument("--trio", type = str)
+parser.add_argument("--twins", type = str)
+parser.add_argument("--info_list", type = str, nargs = '*')
+
 args = parser.parse_args()
-
-mz_twins = {} # Map with first monozygote twin as key, and value is a list where the first value are number of constistencies
- # and the second is the number of inconsistensies
-
-variants = {}
-
 
 def consistency(child_allele_count, mother_allele_count, father_allele_count):
 	if child_allele_count == 0:
@@ -41,7 +37,7 @@ def check_consistency (record, samples, target_allele):
 	else:
 		return False
 
-def statistic_tests(record, fjoldi_samples, target_allele):
+def statistic_tests(record, fjoldi_samples, target_allele, items):
 
 	type_1 = 0
 	consistent_type1 = 0
@@ -97,59 +93,60 @@ def statistic_tests(record, fjoldi_samples, target_allele):
 	o2 = 0
 	o3 = 0
 
-	for line in open(args.trio):
+	if args.trio is not None:
+		for line in open(args.trio):
 
-		samples = line.rstrip().split("\t")
-		child_gt = record.genotype(samples[0])['GT'].split("/")
-		mother_gt = record.genotype(samples[1])['GT'].split("/")
-		father_gt = record.genotype(samples[2])['GT'].split("/")
+			samples = line.rstrip().split("\t")
+			child_gt = record.genotype(samples[0])['GT'].split("/")
+			mother_gt = record.genotype(samples[1])['GT'].split("/")
+			father_gt = record.genotype(samples[2])['GT'].split("/")
 
-		if "." in child_gt or "." in mother_gt or "." in father_gt:
-			continue
+			if "." in child_gt or "." in mother_gt or "." in father_gt:
+				continue
 
-		#fallid check_consistency lista sem telur consistency. Thegar vid erum bara med einn reference tha er annadhvort 1 i consistent eda
-		#1 i inconsistent. THhegar thad eru > 1 reference tha er thetta inconsistent ef thad er amk 1 inconsistent stak.
-		consistency = check_consistency(record, samples, target_allele)
+			#fallid check_consistency lista sem telur consistency. Thegar vid erum bara med einn reference tha er annadhvort 1 i consistent eda
+			#1 i inconsistent. THhegar thad eru > 1 reference tha er thetta inconsistent ef thad er amk 1 inconsistent stak.
+			consistency = check_consistency(record, samples, target_allele)
 
-		f1 = father_gt[0] == str(target_allele)
-		f2 = father_gt[1] == str(target_allele)
-		m1 = mother_gt[0] == str(target_allele)
-		m2 = mother_gt[1] == str(target_allele)
+			f1 = father_gt[0] == str(target_allele)
+			f2 = father_gt[1] == str(target_allele)
+			m1 = mother_gt[0] == str(target_allele)
+			m2 = mother_gt[1] == str(target_allele)
 
-		if (f1 == f2) and (m1 == m2): #skoda hvort thetta se type1
-			type_1 += 1
-			#tjekkum hvort thad se consistent eda inconsistent
-			if consistency:
-				consistent_type1 += 1
-			else:
-				inconsistent_type1 += 1
-
-		elif (f1 == f2) or (m1 == m2): #skoda hvort thetta se type2
-			type_2 += 1
-			#tjekkum hvort thad se consistent eda inconsistent
-			if consistency:
-				consistent_type2 += 1
-				#skodum hversu morg type2 eru heterozygous og hversu morg eru homozygous. ATH inconsistent type 2 flokkad sem heterozygous.
-				if child_gt[0] == child_gt[1]:
-					homozygous_type2 += 1
+			if (f1 == f2) and (m1 == m2): #skoda hvort thetta se type1
+				type_1 += 1
+				#tjekkum hvort thad se consistent eda inconsistent
+				if consistency:
+					consistent_type1 += 1
 				else:
-					heterozygous_type2 +=1
-			else:
-				inconsistent_type2 += 1
-				heterozygous_type2 +=1
+					inconsistent_type1 += 1
 
-		else: #annars er thetta type 3
-			type_3 += 1
-			if consistency:
-				consistent_type3 += 1
-			else:
-				inconsistent_type3 += 1
-			if child_gt[0] != str(target_allele) and child_gt[1] != str(target_allele):
-				o1 += 1
-			elif child_gt[0] == str(target_allele) and child_gt[1] == str(target_allele):
-				o3 += 1
-			else:
-				o2 +=1
+			elif (f1 == f2) or (m1 == m2): #skoda hvort thetta se type2
+				type_2 += 1
+				#tjekkum hvort thad se consistent eda inconsistent
+				if consistency:
+					consistent_type2 += 1
+					#skodum hversu morg type2 eru heterozygous og hversu morg eru homozygous. ATH inconsistent type 2 flokkad sem heterozygous.
+					if child_gt[0] == child_gt[1]:
+						homozygous_type2 += 1
+					else:
+						heterozygous_type2 +=1
+				else:
+					inconsistent_type2 += 1
+					heterozygous_type2 +=1
+
+			else: #annars er thetta type 3
+				type_3 += 1
+				if consistency:
+					consistent_type3 += 1
+				else:
+					inconsistent_type3 += 1
+				if child_gt[0] != str(target_allele) and child_gt[1] != str(target_allele):
+					o1 += 1
+				elif child_gt[0] == str(target_allele) and child_gt[1] == str(target_allele):
+					o3 += 1
+				else:
+					o2 +=1
 
 	#info um chi square tilgatuprofid:
 	#i badum tilvikum er nulltilgatan: No significant difference between observed and expected og gagntilgatan er
@@ -172,52 +169,70 @@ def statistic_tests(record, fjoldi_samples, target_allele):
 	else:
 		chi_statistic_type3 = 'n/a' #thvi vid erum tha ad deila med nulli, spurning um tulkun a thvi 
 
-	twins_passar = 0
-	twins_passar_ekki = 0
-	twins_prosenta = 0
 
-	for lina in open(args.twins):
-		samples_twins = lina.rstrip().split("\t")
-		twin1 = record.genotype(samples_twins[0])['GT'].split("/")
-		twin2 = record.genotype(samples_twins[1])['GT'].split("/")
-		key = samples_twins[0] + samples_twins[1]
+	if args.twins is not None:
+		twins_passar = 0
+		twins_passar_ekki = 0
+		twins_prosenta = 0
+		for lina in open(args.twins):
+			samples_twins = lina.rstrip().split("\t")
+			twin1 = record.genotype(samples_twins[0])['GT'].split("/")
+			twin2 = record.genotype(samples_twins[1])['GT'].split("/")
+			key = samples_twins[0] + samples_twins[1]
 
-		if key not in mz_twins:
-			mz_twins[key] = [0, 0]
+			if key not in mz_twins:
+				mz_twins[key] = [0, 0]
 
-		if twin1.count('1') == twin2.count('1'):
-			twins_passar += 1
-			mz_twins[key][0] += 1
-		else:
-			twins_passar_ekki += 1
-			mz_twins[key][1] += 1
+			if twin1.count('1') == twin2.count('1'):
+				twins_passar += 1
+				mz_twins[key][0] += 1
+			else:
+				twins_passar_ekki += 1
+				mz_twins[key][1] += 1
 
-	twins_prosenta = twins_passar/(twins_passar + twins_passar_ekki)
+		
+		twins_prosenta = twins_passar/(twins_passar + twins_passar_ekki)
+
+		file.write(record.ID + ':' + str(target_allele) + '\t' + str(twins_prosenta))
+		file.write('\n')
+
+	gildi = []
+	if args.info_list is not None:
+		for bla in items:
+			if bla in record.INFO:
+				gildi.append(record.INFO[bla])
 
 	#hendum ollum upplysingunum inn i global dictionary sem geymir thaer
-	variants_key = record.ID + ':' + str(target_allele)
-	variants[variants_key] = [0,0,0,0,0,0,0,0,0,0,0,0,0]
-	variants[variants_key][0] = type_1
-	variants[variants_key][1] = consistent_type1
-	variants[variants_key][2] = inconsistent_type1
-	variants[variants_key][3] = type_2
-	variants[variants_key][4] = consistent_type2
-	variants[variants_key][5] = inconsistent_type2
-	variants[variants_key][6] = chi_statistic_type2
-	variants[variants_key][7] = type_3
-	variants[variants_key][8] = consistent_type3
-	variants[variants_key][9] = inconsistent_type3
-	variants[variants_key][10] = chi_statistic_type3
-	variants[variants_key][11] = harwey_chi
-	variants[variants_key][12] = twins_prosenta
+	trio_listi = []
+	info_listi = []
+	if args.trio is not None:
+		trio_listi = [type_1, consistent_type1, inconsistent_type1, type_2, consistent_type2, inconsistent_type2, chi_statistic_type2, type_3, consistent_type3, inconsistent_type3, chi_statistic_type3]
 
+	prent_var = trio_listi + gildi
+
+	print (record.ID + ':' + str(target_allele), harwey_chi, *prent_var, sep = "\t" )
 #-----------------------------------------------------------------------------------------------------------------
 
 
 vcf_reader = vcf.Reader(open(args.vcf, 'r'))
 
-print ('ID', '#Type 1', "# Consistent Type1", "Inconsistent Type1", "# Type 2", "# Consistent Type2", "Inconsistent Type2", "Type 2 Chi Square Statistic" "# Type 3", "# Consistent Type3", "Inconsistent Type3" "Type 3 Chi Square Statistic", "Variant Hardy Weiberg Chi Square Statistic", "tvibura_hlutfall", sep = "\t" )
+mz_twins = {} # Map with first monozygote twin as key, and value is a list where the first value are number of constistencies
+ # and the second is the number of inconsistensies
 
+variants = {}
+
+items = []
+prent_trio = []
+
+if args.info_list is not None:
+	items = args.info_list
+
+if args.trio is not None:
+	prent_trio = ['#Type 1', "# Consistent Type1", "Inconsistent Type1", "# Type 2", "# Consistent Type2", "Inconsistent Type2", "Type 2 Chi Square Statistic" "# Type 3", "# Consistent Type3", "Inconsistent Type3" "Type 3 Chi Square Statistic"]
+
+prenti_listi = prent_trio + items
+
+print ('ID', "Variant H.W C.S.S", *prenti_listi, sep = "\t" )
 
 fjoldi_samples = len(vcf_reader.samples)
 
@@ -226,14 +241,19 @@ deletion = 0
 insertion = 0
 complx = 0
 
+if args.twins is not None:
+	file = open ("twin_output.txt", "w")
+	file.write('ID' + '\t' + 'Hlutfall')
+	file.write ('\n')
+
+
 for record in vcf_reader:
+
 	if len(record.ALT) == 1:
-		statistic_tests(record, fjoldi_samples, 1)
+		statistic_tests(record, fjoldi_samples, 1, items)
 	else:
 		for i in range(len(record.ALT)):
-			statistic_tests(record, fjoldi_samples, i)
-
-
+			statistic_tests(record, fjoldi_samples, i, items)
 
 	allele_len_1 = sum(len(i) == 1 for i in record.ALT) + int(len(record.REF) == 1)
 
@@ -246,19 +266,11 @@ for record in vcf_reader:
 	else:
 		deletion +=1
 
+if args.twins is not None:
+	file.close()
 
-for variant in variants:
-	print (variant, *variants[variant], sep = '\t')
-
-print ('\n')
-print ('twin', 'hlutfall', sep = '\t')
-for twin in mz_twins:
-	print (twin, mz_twins[twin][0]/(mz_twins[twin][0]+mz_twins[twin][1]), sep = '\t')
-
-print ('Snippuer', 'Insertion', 'Deletion', 'Complex', sep = '\t')
+print ('Snippur', 'Insertion', 'Deletion', 'Complex', sep = '\t')
 print (snippur, insertion, deletion, complx, sep = '\t')
-
-
 
 
 
